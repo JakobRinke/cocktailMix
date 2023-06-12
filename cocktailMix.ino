0f2f45eacc897ada510df49b863dfa3b24c7943c
JakobRinke <61539310+JakobRinke@users.noreply.github.com>
2023-06-12 17:21:06 +0200

    Update cocktailMix.ino

--- cocktailMix.ino
+++ cocktailMix.ino
@@ -1,37 +1,68 @@
 
+// Pumpengeschwindigkeit in ml/s
+const double pumpSpeed = 30;
+
+// Größe des Glases in ml
+const double cupSize = 400;
 
 const uint8_t buttonPins[] =  {2, 3, 4};
 
-const uint8_t ledPins[] = {8, 9, 10, 11};
+const uint8_t pumpPins[] = {8, 9, 10, 11};
 
-const float recipies[sizeof(buttonPins)][sizeof(ledPins)] = {
-  { 4, 0, 2, 4},
-  { 2, 2, 1, 1},
-  { 0, 0, 7, 2},
+// Recipies 
+const double recipies[sizeof(buttonPins)][sizeof(pumpPins)] = {
+  { 0.3, 0.0, 0.4, 0.3},
+  { 0.0, 0.5, 0.1, 0.4},
+  { 0.3, 0.2, 0.0, 0.5},
 };
 
 
 const uint8_t waitPin = 7;
 
+
 void setup() {
+  Serial.begin(9600);
   for(int i = 0; i < sizeof(buttonPins); i++) {
     pinMode(buttonPins[i], INPUT);
   }
   
-  for(int i = 0; i < sizeof(ledPins); i++) {
-    pinMode(ledPins[i}, OUTPUT);
+  for(int i = 0; i < sizeof(pumpPins); i++) {
+    pinMode(pumpPins[i], OUTPUT);
   }
   pinMode(waitPin, OUTPUT);
 
 }
 
+
+
+int calcFIllTime(int button, int i) {
+  return (int) (1000 * recipies[button][i] * cupSize / pumpSpeed);
+}
+
+void putInRow(int button) {
+  for(int i = 0; i < sizeof(pumpPins); i++) {
+    Serial.println(i);
+    if (recipies[button][i] > 0) {
+         digitalWrite(pumpPins[i], HIGH);
+         delay(calcFIllTime(button, i));
+         digitalWrite(pumpPins[i], LOW);
+    }  
+  }
+}
+
+
 void loop() {
-  for (int button = 0; button < sizeof(buttonPins)) {
-    if (digitalRead(buttonPins[button]) == HIGH) {
+  for (int button = 0; button < sizeof(buttonPins); button++) {
+    Serial.print("Test: ");
+    Serial.println(button);
+    if (digitalRead(buttonPins[button]) == LOW) {
+      Serial.println("BASED");
+      digitalWrite(waitPin, HIGH);
+      putInRow(button);
+      delay(3000);
+      digitalWrite(waitPin, LOW);
       break;
     }
   }
-  
-  // put your main code here, to run repeatedly:
-  delay(5)
+  delay(500);
 }
